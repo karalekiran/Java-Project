89ceb79b0e6c228f5210f889171f1c652c66030c
DELL <DELL@DESKTOP-MORDVBI>
2024-03-07 12:30:31 +0530

    Changes are made in class

--- A/src/com/A.java
+++ A/src/com/A.java
@@ -2,9 +2,17 @@ package com;
 
 public class A {
 
+	public void m1() {
+		
+		System.out.println("This is mi method o f class A");
+		
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		A a=new A();
+		a.m1();
+        System.out.println("Welcome to Cjc Classes");
+        
 	}
 
 }
